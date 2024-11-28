0c2f378199b6c3b40723512d454526e3865bbefe
Bagusylyto <byuliyanto1@gmail.com>
2024-11-29 00:20:14 +0700

    uts

--- src/components/Elements/Card/index.jsx
+++ src/components/Elements/Card/index.jsx
@@ -2,10 +2,7 @@ const Card = () => {
   return (
     <>
       <div className="text-lg text-gray-02 mb-2">Title</div>
-      <div className="bg-white rounded-lg px-6 py-5 shadow-xl">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur neque similique sit, obcaecati natus harum debitis asperiores accusantium? Necessitatibus quae repellendus harum ipsam sunt velit laboriosam aliquam exercitationem
-        in deleniti.
-      </div>
+      <div className="bg-white rounded-lg px-6 py-5 shadow-xl">description</div>
     </>
   );
 };
